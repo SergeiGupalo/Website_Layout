b2d2b5b1d9416761eb2153ba13ddc5cd6a9de5fb
SergeiGupalo <sergeig143@gmail.com>
2019-12-17 17:29:09 +0200

    free

---
+++ tajam/js/main.js
@@ -0,0 +1,40 @@
+
+$('.header__slider').slick({
+  nextArrow: '<button type="button" class="slick-btn slick-nex"></button>',
+  prevArrow: '<button type="button" class="slick-btn slick-pre"></button>',
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+  autoplaySpeed: 1500,
+	dots: true
+});
+
+$('.slider').slick({
+  nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+  prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+
+  centerMode: true,
+  centerPadding: '20px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 1
+      }
+    }
+  ]
+
+});
