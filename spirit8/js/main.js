cca30589b3bd95d1dd05ac14ffe7b5a63b5b42df
SergeiGupalo <57807808+SergeiGupalo@users.noreply.github.com>
2020-01-30 01:25:40 +0200

    spirit

---
+++ spirit8/js/main.js
@@ -0,0 +1,25 @@
+$('.slider').slick({
+	slidesToShow: 4,
+	slidesToScroll: 2,
+	autoplay: true,
+  autoplaySpeed: 1500,
+  	arrows: false,
+	dots: true
+});
+
+$('.slider__item').slick({
+	slidesToShow: 4,
+	slidesToScroll: 2,
+	autoplay: true,
+  autoplaySpeed: 1500,
+  	arrows: false,
+	dots: true
+})
+$('.slider__client-intro').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+  autoplaySpeed: 1500,
+  	arrows: false,
+	dots: true
+})
